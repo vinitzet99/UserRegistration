083cfee7f5c509a9f373283c8ddd0a8156728317
vinit <vinitsxn@gmail.com>
2022-01-16 18:31:40 +0530

    Added: Welcome Message added

---
+++ src/main/java/userregistration/UserRegistrationMain.java
@@ -0,0 +1,18 @@
+/**
+ * @author: Vinit Kumar
+ * @Created on: 16/01/2022
+ * User Registration
+ */
+package userregistration;
+
+public class UserRegistrationMain {
+
+    //welcome message
+    public static void welcome() {
+        System.out.println("Welcome to User Registration System Problem");
+    }
+
+    public static void main(String[] args) {
+        welcome();
+    }
+}
